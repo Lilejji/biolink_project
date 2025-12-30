e38316fc4d9efa09eb20c0f8641b59c9d32dfcac
Lilejji <wassim.hip123@gmail.com>
2025-12-30 19:43:13 +0100

    Add whitenoise and fix deploy

--- links/urls.py
+++ links/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 from .views import ProfileDetailView, SignUpView, DashboardView, EditProfileView, add_link, delete_link, reorder_links
 
 urlpatterns = [
     # --- SPECIFIC PAGES MUST BE FIRST ---
-    
-    # User Auth & Dashboard
+    path('', TemplateView.as_view(template_name='links/index.html'), name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='links/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
